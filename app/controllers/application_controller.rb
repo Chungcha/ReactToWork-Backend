fc4ce573d6c44678df703467d7183c981d2436f7
Adam Finch <atyreefinch@gmail.com>
2019-12-02 13:25:58 -0500

    hiding secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
 
     def encode_token(payload)
         # payload => { beef: 'steak' }
-        JWT.encode(payload, 'my_s3cr3t')
+        JWT.encode(payload, ENV['oath_key'])
         
       end
      
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::API
           token = auth_header.split(' ')[1]
           # headers: { 'Authorization': 'Bearer <token>' }
           begin
-            JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+            JWT.decode(token, ENV['oath_key'], true, algorithm: 'HS256')
             # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
           rescue JWT::DecodeError
             nil
