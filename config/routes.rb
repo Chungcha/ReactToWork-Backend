fde204ad9d7eb344d7df2ad4c15bbc77da48cf3b
chungcha <charlieschung@gmail.com>
2019-12-02 12:36:05 -0500

    renamed keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :jobs
+  get "/remote", to: "jobs#remote"
   resources :users, only: [:create, :delete, :update, :show]
   resources :saves, only: [:create, :delete]
 end
