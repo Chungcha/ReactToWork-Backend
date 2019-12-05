e90462628b9cb732caec4b65ad7a36041b10e52c
chungcha <charlieschung@gmail.com>
2019-12-05 14:25:38 -0500

    i think self join done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,49 +5,49 @@ class ApplicationController < ActionController::API
     Rails.application.secrets.secret_key_base
   end 
 
-    def encode_token(payload)
-        # payload => { beef: 'steak' }
-        JWT.encode(payload, secret_key)
-      end
+  def encode_token(payload)
+    # payload => { beef: 'steak' }
+    JWT.encode(payload, secret_key)
+  end
      
-      def auth_header
-        # { 'Authorization': 'Bearer <token>' }
-        request.headers['Authorization']
-      end
+  def auth_header
+    # { 'Authorization': 'Bearer <token>' }
+    request.headers['Authorization']
+  end
      
-      def decoded_token
-        if auth_header
-          token = auth_header.split(' ')[1]
-          # headers: { 'Authorization': 'Bearer <token>' }
-          begin
-            JWT.decode(token, secret_key, true, algorithm: 'HS256')
-            # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
-          rescue JWT::DecodeError
-            nil
-          end
-        end
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # headers: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, secret_key, true, algorithm: 'HS256')
+        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+      rescue JWT::DecodeError
+        nil
       end
+    end
+  end
 
-      def decode(token)
-        JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
-        #return OG payload
-    end 
+  def decode(token)
+    JWT.decode(token, secret_key, true, {algorithm: 'HS256'})[0]
+    #return OG payload
+  end 
 
-        def current_user
-            if decoded_token
-              # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
-              # or nil if we can't decode the token
-              user_id = decoded_token[0]['user_id']
-              @user = User.find_by(id: user_id)
-            end
-          end
+  def current_user
+    if decoded_token
+      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
+      # or nil if we can't decode the token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
          
-          def logged_in?
-            !!current_user
-          end
+  def logged_in?
+    !!current_user
+  end
 
-          def authorized
-            render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
-          end
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 
 end
--- db/migrate/20191202150649_create_jobs.rb
+++ db/migrate/20191202150649_create_jobs.rb
@@ -6,8 +6,9 @@ class CreateJobs < ActiveRecord::Migration[6.0]
       t.string :link
       t.string :position
       t.string :zipCode
-      t.array :category
+      t.string :category
       t.datetime :date
+      t.integer :poster_id
       
       t.timestamps
     end
